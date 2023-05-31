6960a8ed1e0afc482fb6d18050e7e7d3f2741405
edoardorizzo <edo96.rizzo@gmail.com>
2023-05-31 14:47:15 +0200

    refactoring extra code

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,12 +1,11 @@
 <header id="app_header">
-  @include("partials.topHeader")
-
+  @include("resoruces.views.partials.topHeader")
   <div id="header_links">
     <div class="container d-flex justify-content-between align-items-center">
       <div id="logo_wrapper">
         <img height="80" src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="DC Logo">
       </div>
-      @include("partials.headerLinks")
+      @include("resoruces.views.partials.headerLinks")
       <div id="search_wrapper" class="position-relative">
         <input type="search" placeholder="Search" name="searchComics" id="searchComics" class="border-primary">
         <i class="fa-solid fa-magnifying-glass fa-fw" id="searchIcon"></i>
